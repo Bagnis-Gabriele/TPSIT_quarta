1ac463904eb66fd391f0469e5b7a2847af9956d8
Gabriele Bagnis <gabriele.bagnis@itiscuneo.eu>
2020-04-04 13:23:23 +0200

    add es5 thread

--- 24.Esercizio_thread_5/es5.c
+++ 24.Esercizio_thread_5/es5.c
@@ -4,13 +4,26 @@
 #include <unistd.h>
 #include <time.h>
 
-int biglietti;
-pthread_mutex_t mcassa1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mcassa2 = PTHREAD_MUTEX_INITIALIZER;
+int biglietti, successivo, cassa1, cassa2=0;
+pthread_mutex_t mcassa = PTHREAD_MUTEX_INITIALIZER;
 
-void compraBiglietto(int cassa){
-    int numero= rand() % 9 + 1;
+void cassa(void *arg){
+    int cassa=*((int*)arg);
+    if(cassa==1){
+        cassa1++;
+    }else{
+        cassa2++;
+    }
+    int exit=1;
+    int numero= rand() % 10;
     int acquistati = 0;
+    while(exit==1){
+        if((cassa==1&&successivo==1&&pthread_mutex_lock(&mcassa)==0)||cassa2==0){
+            exit=0;
+        }else if ((cassa==2&&successivo==2&&pthread_mutex_lock(&mcassa)==0)||cassa1==0){
+            exit=0;
+        }
+    }
     if (biglietti >= numero){
         biglietti-= numero;
         acquistati= numero;
@@ -18,38 +31,38 @@ void compraBiglietto(int cassa){
         acquistati=biglietti;
         biglietti=0;
     }
-    printf("CASSA: %d sono il cliente %u e vorrei acquistare %d biglietti\n\tbiglietti acquistati: %d\n\tbiglietti disponibili: %d\n", cassa, pthread_self() , numero, acquistati, biglietti);
-    return;
-}
-
-void cassa1(void *arg){
-    pthread_mutex_lock(&mcassa1);
-    compraBiglietto(1);
-    pthread_mutex_unlock(&mcassa1);
-
-    pthread_exit(0);
-}
-
-void cassa2(void *arg){
-    pthread_mutex_lock(&mcassa2);
-    compraBiglietto(2);
-    pthread_mutex_unlock(&mcassa2);
+    successivo=rand() % 10;
+    if(successivo>5){
+        successivo=1;
+    }else{
+        successivo=2;
+    }
+    pthread_mutex_unlock(&mcassa);
+    if(cassa==1){
+        cassa1--;
+    }else{
+        cassa2--;
+    }
+    printf("CASSA: %d sono il cliente %u e vorrei acquistare %d biglietti\n\tbiglietti acquistati: %d\n\tbiglietti disponibili: %d\n\tcassa successiva: %d\n", cassa, pthread_self() , numero, acquistati, biglietti, successivo);
 
     pthread_exit(0);
 }
 
 int main(int argc, char **argv){
     srand(time(NULL));
-    biglietti=100;
+    biglietti=500;
+    successivo=1;
     int n_clienti=100;
     pthread_t clienti[n_clienti];
     int i;
 
     for(i=0; i<n_clienti; i++){
+        int *x=(int*)malloc(sizeof(int));
+        *x=(i%2)+1;
         if(i%2==0){
-            pthread_create(&clienti[i], NULL, (void*) &cassa1, NULL);
+            pthread_create(&clienti[i], NULL, (void*) &cassa, (void *) x);
         }else{
-            pthread_create(&clienti[i], NULL, (void*) &cassa2, NULL);
+            pthread_create(&clienti[i], NULL, (void*) &cassa, (void *) x);
         }
     }
 
